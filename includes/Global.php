272d44efb23eb7a16ef96c3b8296a220bc74ab04
jose_vs1 <jose_vs1@SNSOO1EP01VM168.saojose.sc.senai.br>
2015-11-17 10:33:54 -0200

    Criação da Conexao com o banco de dados.

--- includes/Global.php
+++ includes/Global.php
@@ -1,8 +1,13 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+session_start();
+
+function conexao(){
+    $user = "root";
+    $passwd = "usbw";
+    $dbname = "estoque";
+    $host = "127.0.0.1";
+    $pdo = new PDO("mysql:host={$host};dbname={$dbname}", $user, $passwd);
+    return$pdo;
+}
 
