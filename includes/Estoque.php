becc8b8f0784f6f92ba63928cb45bb905f37fce9
jose_vs1 <jose_vs1@SNSOO1EP01VM168.saojose.sc.senai.br>
2015-11-17 11:46:17 -0200

    criação de partes do arquivo estoque.php

---
+++ includes/Estoque.php
@@ -0,0 +1,32 @@
+<?php
+if(file_exists("Global.php")){
+include_once './Global.php';
+}else if(file_exists("includes/Global.php")){
+   include_once './includes/Global.php'; 
+}
+function salvar(){
+    if(
+            isset($_POST['nome'])and
+            isset($_POST['valor'])and 
+            isset($_POST['qtd'])
+            )
+    {
+                $nome = $POST['nome'];
+                $valor = $POST['valor'];
+                $qtd = $POST['qtd'];
+                
+                $SQL = "";
+                $preparo = conexao()-> prepare($SQL);
+                $preparo->bindValue("", $nome);
+                $preparo->bindValue("", $valor);
+                $preparo->bindValue("", $qtd);
+                $preparo->execute();
+                if($preparo->rowCount()== 1){
+                    echo"Sucesso!";
+                }  else {
+                echo"Erro!";
+                }
+                
+    }
+}
+
