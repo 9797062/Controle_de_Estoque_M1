50a508f56bf6a14299f1e0ff1dfb713b7589a283
jose_vs1 <jose_vs1@SNSOO1EP01VM160.saojose.sc.senai.br>
2015-11-23 09:31:42 -0200

    criação e modificaçãoda função adicionar produto na base de dados. alteração do dbname no arquivo global.php

--- includes/Estoque.php
+++ includes/Estoque.php
@@ -8,18 +8,20 @@ function salvar(){
     if(
             isset($_POST['nome'])and
             isset($_POST['valor'])and 
-            isset($_POST['qtd'])
+            isset($_POST['Quantidade'])
             )
     {
-                $nome = $POST['nome'];
-                $valor = $POST['valor'];
-                $qtd = $POST['qtd'];
+                $nome = $_POST['nome'];
+                $valor = $_POST['valor'];
+                $Quantidade = $_POST['Quantidade'];
+                $DataDeValidade = (isset($_POST['DataDeValidade']) ? $_POST['DataDeValidade'] :"" );
                 
-                $SQL = "";
+                $SQL = "INSERT INTO produto(nome,valor,Quantidade,DataDeValidade)values(:nome, :valor, :Quantidade, :DataDeValidade)";
                 $preparo = conexao()-> prepare($SQL);
-                $preparo->bindValue("", $nome);
-                $preparo->bindValue("", $valor);
-                $preparo->bindValue("", $qtd);
+                $preparo->bindValue("nome", $nome);
+                $preparo->bindValue("valor", $valor);
+                $preparo->bindValue("Quantidade", $Quantidade);
+                $preparo->bindValue("DataDeValidade", $DataDeValidade);
                 $preparo->execute();
                 if($preparo->rowCount()== 1){
                     echo"Sucesso!";
--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <?php
 include './includes/Global.php';
+include './includes/Estoque.php';
 $title = 'controle de estoque';
 include './template/header.php';
 ?>
+<?php salvar(); ?>
 <form method="post">
     <div class="center">
+        <h2 style="text-align: center;">Cadastro de Produtos</h2>
         Nome: <input type="text" name="nome"/>
         Valor: <input type="text" name="valor"/>
         Quantidade: <input type="text" name="Quantidade"/>
